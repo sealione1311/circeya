c152ce8d0872f1441f0b7dc9cddae9cefb586a33
Tatyana Mirzova <44976901+sealione1311@users.noreply.github.com>
2023-02-09 11:51:20 +0300

    add swipe

--- src/components/scrollSection/ScrollSection.jsx
+++ src/components/scrollSection/ScrollSection.jsx
@@ -6,16 +6,18 @@ const ScrollSection = () => {
     const slider = useRef(null);
     const scrollWrapper = useRef(null);
     const scrollSection = useRef(null);
+    let windowWidth = window.innerWidth;
+    let x = null;
+    let currentSliderX = 0;
 
-    useEffect(function () {
+    const onScroll = () => {
         if (
-            slider.current
+            slider.current && windowWidth > 821
         ) {
-            let windowWidth = window.innerWidth;
+
             let horLength = slider.current.scrollWidth;
             let distFromTop = scrollWrapper.current.offsetTop;
             let scrollDistance = distFromTop + horLength - windowWidth;
-
             scrollWrapper.current.style.height = horLength + "px";
 
             window.onscroll = function () {
@@ -27,6 +29,39 @@ const ScrollSection = () => {
                 }
             }
         }
+    }
+    const onTouchStart = (evt) => {
+        x = evt.touches[0].clientX;
+        console.dir(slider.current.scrollWidth)
+    }
+
+    const onTouchMove = (evt) => {
+        let touch = evt.touches[0];
+        let diff = x - touch.clientX;
+        if (diff < 0) {
+            slider.current.style.transform = `translateX(${currentSliderX+Math.abs(diff)}px)`;
+            currentSliderX = (slider.current.style.transform.slice(11, -3));
+            if(currentSliderX > 0 ) {
+                return
+            }
+        }
+        if (currentSliderX < -(slider.current.scrollWidth - windowWidth) ) {
+            currentSliderX = -(slider.current.scrollWidth - windowWidth)
+            return
+        }
+        slider.current.style.transform = `translateX(${(currentSliderX - diff)}px)`;
+        currentSliderX = (slider.current.style.transform.slice(11, -3));
+    }
+    const onTouchEnd = () => {
+        slider.current.style.transform = `translateX(${(currentSliderX)}px)`;
+    }
+
+    useEffect(function () {
+        window.addEventListener("scroll", onScroll);
+
+        return function () {
+            window.removeEventListener("scroll", onScroll);
+        };
     }, []);
 
     return (
@@ -36,7 +71,11 @@ const ScrollSection = () => {
                     <h1 className={styles.scrollTitle}>
                         Lorem ipsum dolor sit amet
                     </h1>
-                    <div className={styles.stickyWrapper}>
+                    <div className={styles.stickyWrapper}
+                         onTouchStart={onTouchStart}
+                         onTouchMove={onTouchMove}
+                         onTouchEnd={onTouchEnd}
+                    >
                         <div className={styles.slider} ref={slider}>
                             {sliders.map((img) => (
                                 <div
